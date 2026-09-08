3250690fa333ab956385e8d8fa6ecb90dcfa2db2
CAMILLE BOUYRIE--FLEURIGEON <camille.bouyrie--fle@iut.local>
2026-09-08 17:36:19 +0200

    résolution pb variables eng!=fr

--- LectureEcritureJava/Image.java
+++ LectureEcritureJava/Image.java
@@ -2,6 +2,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Image {
+    
+    //Nous aurions pu remplacer les int en byte afin de gagner en place mémoire.
     private int width;
     private int height;
     // pixels[y][x][0=R,1=G,2=B]
@@ -13,17 +15,17 @@ public class Image {
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int wigth, int height) {
         this.width = width;
         this.height = height;
-        pixels = new int[hauteur][largeur][3];
+        pixels = new int[height][width][3];
     }
 
     /**
      * Définit la couleur d'un pixel à la position (x, y)
      */
     public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
