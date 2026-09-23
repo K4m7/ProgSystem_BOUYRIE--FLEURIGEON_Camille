7439027f8df1bd95263189dcdf09e383bca3f3db
K4m7 <camille.bouyrie@gmail.com>
2026-09-23 21:00:14 +0200

    Utils.java : Correction BigEndian + tests Utils.java + Etape2 done

--- SystemeFichierVirtuel/Utils.java
+++ SystemeFichierVirtuel/Utils.java
@@ -2,10 +2,10 @@ public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
 		
-		byte b1 = (byte) (value & 0xFF);
-		byte b2 = (byte) (value >> 8 & 0xFF);
-		byte b3 = (byte) (value >> 16 & 0xFF);
-		byte b4 = (byte) (value >> 24 & 0xFF);
+		byte b1 = (byte) (value >> 24 & 0xFF);
+		byte b2 = (byte) (value >> 16 & 0xFF);
+		byte b3 = (byte) (value >> 8 & 0xFF);
+		byte b4 = (byte) (value & 0xFF);
 		
 		memory[offset] = b1;
 		memory[offset + 1] = b2;
@@ -16,23 +16,23 @@ public class Utils {
 	}
 
     public static int readInt(byte[] memory, int offset) {
-		
-        int b1 = memory[offset] & 0xFF;
-        int b2 = memory[offset + 1] & 0xFF;
-        int b3 = memory[offset + 2] & 0xFF;
-        int b4 = memory[offset + 3] & 0xFF;
+
+        int b1 = memory[offset + 3] & 0xFF;
+	    int b2 = memory[offset + 2] & 0xFF;
+        int b3 = memory[offset + 1] & 0xFF;
+        int b4 = memory[offset] & 0xFF;
 
         return b1 | (b2 << 8) | (b3 << 16) | (b4 << 24);
     }
     public static int writeShort(byte[] memory, int offset, short value) {
-        memory[offset] = (byte) (value & 0xFF);
-        memory[offset + 1] = (byte) ((value >> 8) & 0xFF);
+        memory[offset] = (byte) ((value >> 8) & 0xFF);
+		memory[offset +1] = (byte) (value & 0xFF);
         return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
-        int b1 = memory[offset] & 0xFF;
-        int b2 = memory[offset + 1] & 0xFF;
+        int b1 = memory[offset + 1] & 0xFF;
+		int b2 = memory[offset] & 0xFF;
 
         return (short) (b1 | (b2 << 8));
     }
