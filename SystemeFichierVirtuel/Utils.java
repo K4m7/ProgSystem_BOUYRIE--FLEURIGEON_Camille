5dc68c73280161413b452c2009147d27fd02b59c
K4m7 <camille.bouyrie@gmail.com>
2026-09-23 21:37:54 +0200

    Done : writeLong() + readLong() + writeString() + correction writeInt() + TODO : test étape 3 + readString()

--- SystemeFichierVirtuel/Utils.java
+++ SystemeFichierVirtuel/Utils.java
@@ -1,17 +1,10 @@
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
-		
-		byte b1 = (byte) (value >> 24 & 0xFF);
-		byte b2 = (byte) (value >> 16 & 0xFF);
-		byte b3 = (byte) (value >> 8 & 0xFF);
-		byte b4 = (byte) (value & 0xFF);
-		
-		memory[offset] = b1;
-		memory[offset + 1] = b2;
-		memory[offset + 2] = b3;
-		memory[offset + 3] = b4;
-		
+		memory[offset] = (byte) (value >> 24 & 0xFF);
+		memory[offset + 1] = (byte) (value >> 16 & 0xFF);
+		memory[offset + 2] = (byte) (value >> 8 & 0xFF);
+		memory[offset + 3] = (byte) (value & 0xFF);
 		return 4;
 	}
 
@@ -36,4 +29,59 @@ public class Utils {
 
         return (short) (b1 | (b2 << 8));
     }
+	
+	public static int writeLong(byte[] memory, int offset, long value) {
+		memory[offset] = (byte) (value >> 56 & 0xFF);
+		memory[offset + 1] = (byte) (value >> 48 & 0xFF);
+		memory[offset + 2] = (byte) (value >> 40 & 0xFF);
+		memory[offset + 3] = (byte) (value >> 32 & 0xFF);
+		memory[offset + 4] = (byte) (value >> 24 & 0xFF);
+		memory[offset + 5] = (byte) (value >> 16 & 0xFF);
+		memory[offset + 6] = (byte) (value >> 8 & 0xFF);
+		memory[offset + 7] = (byte) (value & 0xFF);
+		return 8;
+	}
+
+	public static long readLong(byte[] memory, int offset) {
+		long sortie = 0;
+		for (int position = 0; position < 8; position++) {
+			int decalage = (7-position) * 8;
+			long octet = memory[offset+position] & 0xFF;
+			sortie += (octet << decalage);
+		}
+		return sortie;
+	}
+
+	public static int writeString(
+			byte[] memory,
+			int offset,
+			String str,
+			int maxLength) {
+
+		byte[] strToBytes = str.getBytes();
+		int octetsACopier = Math.min(strToBytes.length, maxLength); //car si str < maxLength alors dépasse et si str > maxLength alors on garde que maxLength
+		
+		for (int octet = 0; octet < octetsACopier; octet++) {
+			memory[offset + octet] = strToBytes[octet];
+		}
+
+		for (int octet = octetsACopier; octet < maxLength; octet++) {
+			memory[offset + octet] = 0;
+		
+		}
+		
+		return maxLength;
+	}
+
+	public static String readString(
+			byte[] memory,
+			int offset,
+			int maxLength) {
+
+		// TODO:
+		// Lire jusqu'au premier octet nul
+		// ou jusqu'à maxLength.
+
+		return "";
+	}
 }
